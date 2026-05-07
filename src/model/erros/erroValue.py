c1682ce8446060185a5a79866614ecb577e90796
GabrielRossi98 <gabrielrossi2508@gmail.com>
2026-05-07 12:04:05 -0300

    Valor
    ValueError

---
+++ src/model/erros/erroValue.py
@@ -0,0 +1,8 @@
+
+class ErroValue(Exception):
+    def __init__(self,mensagem):
+        super().__init__(mensagem)
+
+class ErroValorNegativo(Exception):
+    def __init__(self, mensagem):
+        super().__init__(mensagem)
---
+++ src/model/valueObjects/valor.py
@@ -0,0 +1,18 @@
+from src.model.erros.erroValue import ErroValue,ErroValorNegativo
+class Valor:
+
+    def __init__(self,valor:float):
+        self.__valor=valor
+
+
+    def validarValor(self):
+
+        if isinstance(self.__valor,float):
+            if self.__valor>0:
+                return self.__valor
+            else:
+                raise ErroValorNegativo("O valor deve ser positivo")
+
+
+        else:
+            raise ErroValue("Você deve digitar um valor decimal")
